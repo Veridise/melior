c62eea50546ff1423d480e13461d6cb41528e58b
Daniel Dominguez <daniel@veridise.com>
2025-06-23 22:13:11 +0200

    Lower llvm version in macro crate

--- macro/build.rs
+++ macro/build.rs
@@ -1,6 +1,6 @@
 use std::{env, error::Error, path::Path, process::Command, str};
 
-const LLVM_MAJOR_VERSION: usize = 20;
+const LLVM_MAJOR_VERSION: usize = 18;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let version_variable = format!("MLIR_SYS_{}0_PREFIX", LLVM_MAJOR_VERSION);
